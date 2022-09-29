bbee576e07b458925589ad4fcae78ec4f2ef9580
astrit <am@astrit.co>
2022-09-29 16:41:52 +0200

    Send it

---
+++ components/search.jsx
@@ -0,0 +1,4 @@
+// https://codesandbox.io/s/search-page-1-forked-2onjek
+// https://codesandbox.io/s/search-page-1-forked-2onjek
+// https://codesandbox.io/s/search-page-1-forked-2onjek
+// https://codesandbox.io/s/search-page-1-forked-2onjek
