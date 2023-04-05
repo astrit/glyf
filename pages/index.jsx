e0817da66ab3f0a82b1bafb4595f527dc6a99c5b
astrit <am@astrit.co>
2023-04-05 23:40:01 +0200

    Yayy working version

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,8 +6,8 @@ import Glyphs from "g/list";
 import SVGMap from "g/svg/svg";
 import Footer from "@/footer";
 import Carbon from "u/ads";
-import Search from "@/search/three";
-// import Search from "@/search/search";
+// import Search from "@/search/three";
+import Search from "@/search/search";
 import Hero from "@/hero";
 
 const Main = styled("main", {
