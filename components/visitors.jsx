43e5f0e2a08b3226ae9898c10c363681f03462c3
astrit <am@astrit.co>
2023-04-20 10:31:32 +0200

    Msg → Thu Apr 20 10:31:32 CEST 2023

--- components/visitors.jsx
+++ components/visitors.jsx
@@ -1,21 +1,41 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-// import { Server } from 'Socket.IO'
 
 const ActiveVisitors = () => {
   const [activeVisitors, setActiveVisitors] = useState(0);
 
   useEffect(() => {
     const socket = io("https://glyphs.css.gg");
+
+    socket.on("connect", () => {
+      console.log("Socket connected");
+    });
+
     socket.on("activeVisitors", (count) => {
+      console.log(`Received activeVisitors event: ${count}`);
       setActiveVisitors(count);
     });
+
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected");
+      setActiveVisitors(0);
+    });
+
+    socket.on("connect_error", (error) => {
+      console.error("Socket connection error:", error);
+    });
+
+    socket.on("connect_timeout", (timeout) => {
+      console.error("Socket connection timeout:", timeout);
+    });
+
     return () => {
+      console.log("Disconnecting socket");
       socket.disconnect();
     };
   }, []);
 
-  return <>{activeVisitors}</>;
+  return <div>Active Visitors: {activeVisitors}</div>;
 };
 
 export default ActiveVisitors;
