8a4b66e1eaef6cd70c26beac713c0e005da12c66
astrit <am@astrit.co>
2023-05-06 02:07:03 +0200

    Msg → Sat May  6 02:07:03 CEST 2023

--- utils/ads.jsx
+++ utils/ads.jsx
@@ -14,15 +14,11 @@ const Ads = styled("div", {
   fontFamily: "sans-serif",
   letterSpacing: "initial",
   borderRadius: "8px",
+  zIndex: 1000,
   boxShadow:
     "rgb(0 0 0 / 10%) 2px 3px 8px, rgb(0 0 0 / 40%) -4px 28px 12px -8px",
   color: "rgb(14, 12, 27)",
-  "@sm": {
-    bottom: 0,
-    right: 0,
-    width: "100%",
-    borderRadius: 0,
-  },
+
   ".carbon-wrap": {
     display: "flex",
     flexDirection: "column",
@@ -44,6 +40,43 @@ const Ads = styled("div", {
     gap: "10px",
     flexDirection: "column",
   },
+
+  "@sm": {
+    bottom: 0,
+    right: 0,
+    width: "100%",
+    borderRadius: 0,
+    background: "rgba(255,255,255,0.7)",
+    backdropFilter: "blur(10px)",
+
+    ".carbon-wrap": {
+      flexDirection: "row",
+      position: "relative",
+    },
+
+    ".carbon-img": {
+      width: "40px",
+      height: "40px",
+
+      img: {
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+        objectPosition: "center",
+      },
+    },
+
+    ".carbon-text": {
+      display: "flex",
+      alignItems: "center",
+    },
+
+    ".carbon-poweredby": {
+      position: "absolute",
+      bottom: "0",
+      right: "0",
+    },
+  },
 });
 
 export default function Carbon() {
@@ -59,11 +92,7 @@ export default function Carbon() {
     if (ads) {
       ads.appendChild(script);
     }
-  }, [setAds]);
+  }, []);
 
-  return (
-    <Ads>
-      <div id="ads"></div>
-    </Ads>
-  );
+  return <Ads id="ads" />;
 }
