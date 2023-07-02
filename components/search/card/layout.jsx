628758d4bea90d1758f188a3d2d8b3e4c04e2c67
astrit <am@astrit.co>
2023-07-02 23:48:16 +0200

    Msg → Sun Jul  2 23:48:16 CEST 2023

--- components/search/card/layout.jsx
+++ components/search/card/layout.jsx
@@ -1,5 +1,23 @@
-import { styled } from "@/theme";
-import Link from "next/link";
+import { keyframes, styled } from "@/theme";
+
+const flare = keyframes({
+  "0%": {
+    // backgroundSize: "100%",
+    backgroundPosition: "center left",
+    opacity: 0,
+  },
+  "50%": {
+    // backgroundSize: "200%",
+    backgroundPosition: "center center",
+    opacity: 1,
+  },
+  "100%": {
+    // backgroundSize: "100%",
+    backgroundPosition: "center right",
+    opacity: 0,
+    visibility: "hidden",
+  },
+});
 
 const Layout = styled("a", {
   // const CardStyled = styled(NextLink, {
@@ -18,6 +36,7 @@ const Layout = styled("a", {
   backgroundColor: `hsla(260, 74%, 53%, 1.0)`,
   border: "2px solid hsla(259, 73%, 56%, 1.0)",
   transition: "all 0.8s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
+  cursor: "pointer",
   boxShadow:
     "2px 3px 8px rgba(0, 0, 0, 0.06), 0px 28px 12px -8px rgba(0, 0, 0, 0.04)",
   boxSizing: "border-box",
@@ -29,7 +48,8 @@ const Layout = styled("a", {
   "&:hover, &.selected": {
     color: "$white_alpha100",
     backgroundColor: "hsla(260, 74%, 56%, 1.0)",
-    borderColor: "hsla(209, 99%, 67%, 0.2)",
+    borderColor: "hsla(209, 99%, 100%, 0.2)",
+    // borderColor: "hsla(209, 99%, 67%, 0.2)",
     borderRadius: "24px",
 
     span: {
@@ -56,8 +76,11 @@ const Layout = styled("a", {
     boxShadow:
       "2px 3px 8px rgba(0, 0, 0, 0.06), 0px 28px 12px -8px rgba(0, 0, 0, 0.04)",
     transition: "all 0.8s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
-    background:
-      "linear-gradient(90deg, hsla(209, 99%, 67%, 1.0) 0%, #b447eb 95%)",
+    backgroundImage:
+      "linear-gradient(230deg, hsla(209, 99%, 100%, 0), hsla(262, 72%, 100%, 1), hsla(209, 99%, 100%, 0))",
+    // "linear-gradient(90deg, hsla(209, 99%, 67%, 1.0) 0%, #b447eb 50%)",
+    backgroundSize: "400%",
+    backgroundPosition: "center center",
     width: "100%",
     height: "100%",
     zIndex: "-1",
@@ -66,17 +89,23 @@ const Layout = styled("a", {
     // display: "none",
     visibility: "hidden",
     transition: "all 0.8s cubic-bezier(0.175, 0.885, 0.32, 1.275)",
-    transitionDelay: "all 1s",
+    // transitionDelay: "all 1s",
   },
 
   "&:hover::after, &.selected::after": {
     // display: "flex",
     visibility: "visible",
-    transform: "scale3d(1.12,1.12,1.12)",
+    "--size": "calc(100% + 8px)",
+    // transform: "scale3d(var(--size),var(--size),var(--size))",
+    width: "var(--size)",
+    height: "var(--size)",
     borderRadius: "24px",
     cursor: "pointer",
-    background:
-      "linear-gradient(100deg, hsla(209, 99%, 67%, 1.0) 0%, #b447eb 95%)",
+    animation: `${flare} 1424ms linear forwards`,
+    // animation: `${flare} 424ms cubic-bezier(.16,1,.3,1) forwards`,
+    // animationDelay: "2s",
+    // backgroundImage:
+    //   "linear-gradient(100deg, hsla(209, 99%, 67%, 1.0) 0%, #b447eb 95%)",
   },
   "&:active::after": {
     transform: "scale3d(1.02, 1.02, 1.02)",
