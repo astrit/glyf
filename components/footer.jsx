41cb2f94eeb1712f733b750041e600f1e531f01d
astrit <am@astrit.co>
2023-07-28 13:42:26 +0200

    Msg → Fri Jul 28 13:42:26 CEST 2023

--- components/footer.jsx
+++ components/footer.jsx
@@ -11,7 +11,7 @@ const Layout = styled("footer", {
   display: "flex",
   padding: "8vh 20px",
   fontSize: "14px",
-  gap: "10px",
+  gap: "20px",
   alignItems: "center",
   fontWeight: "300",
 
@@ -21,6 +21,45 @@ const Layout = styled("footer", {
       width: "22px",
       height: "22px",
     },
+  },
+});
+
+const MadeIn = styled("div", {
+  display: "flex",
+  color: "rgba(255,255,255,0.5)",
+  gap: "8px",
+  alignItems: "center",
+  // flex: "0 1 1 auto",
+  fontSize: "10px",
+  letterSpacing: "normal",
+});
+
+const Sweden = styled("div", {
+  boxSizing: "border-box",
+  position: "relative",
+  display: "block",
+  width: "21px",
+  height: "14px",
+
+  "&::after, &::before": {
+    content: "",
+    display: "block",
+    boxSizing: "border-box",
+    position: "absolute",
+    height: "6px",
+    background: "currentColor",
+    boxShadow: "0 8px 0",
+    top: "0",
+  },
+
+  "&::before": {
+    left: 0,
+    width: "7px",
+  },
+
+  "&::after": {
+    width: "12px",
+    right: 0,
   },
 });
 
@@ -35,7 +74,11 @@ export default function Footer() {
           alt="Astrit"
         />
       </Link>
-      © {`${new Date().getFullYear()}`} — Software made in Sweden
+      © {`${new Date().getFullYear()}`}
+      <MadeIn>
+        Made in Sweden
+        <Sweden />
+      </MadeIn>
       {/* <Contributions username="astrit" /> */}
       {/* <Contributions username="astrit" date="2023-05-06" /> */}
     </Layout>
